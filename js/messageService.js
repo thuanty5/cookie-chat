283c19ccf714d58170fd103075fc91e8bee35dbd
Thuan <thuan@Thuans-MacBook-Pro.local>
2017-09-07 09:38:46 -0500

    somewhat done

--- js/messageService.js
+++ js/messageService.js
@@ -1,19 +1,50 @@
+
+
+
 angular.module('chatroom').service('messageService', function($http){
+
   //Here you'll need to create two methods. One called postMessage and the other called getMessages.
 
   //On the lines below create a getMessages method. This method will retrieve data from the backend.
   //The url for the get request should be 'https://practiceapi.devmountain.com/api/chats'
   //Be sure to return whatever gets returned from $http so you can call .then in your controller.
 
+  var self = this;
+  var baseUrl = "https://practiceapi.devmountain.com/api/chats"
 
+  // this.getMessages = function(){
+  //   var promise = $http({
+  //     method: "GET",
+  //     url: baseUrl
+  //   })
+  //   return promise.then(function(response){
+  //     self.messages = response;
+  //     return self.messages;
+  //   });
+  // }
 
+  // this.getMessages = function(){
+  //   return $http.get(baseUrl).then(function(response){
+  //     self.messages = response.data.reverse();
+  //     self.test = response.data[0].createdAt;
+  //     return self.messages;
+  //   });
+  // }
+
+  this.getMessages = function(){
+    return $http.get(baseUrl).then(function(response){
+      self.messages = response.data.reverse();
+      return self.messages;
+    });
+  }
 
   //On the line below create the postMessage method. This method will add data to the backend server.
   //The url for the request needs to be 'https://practiceapi.devmountain.com/api/chats'
   //Because we're making a POST request, need a data object with a message property IE data: {message: "Hello World"}
-  //Also, remember that $http returns a promise. So return the whole $http call (return $http(...)), so you can then use .then in your controller.
+  // //Also, remember that $http returns a promise. So return the whole $http call (return $http(...)), so you can then use .then in your controller.
 
-
-
+  this.postMessage = function(message){
+    $http.post(baseUrl, {message: message})
+  }
 
 });
